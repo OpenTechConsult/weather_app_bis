51d3d7906d4a7dffe21b0097e11df6d9ab6ea9da
OpenTech-Consult <opentech.consult@gmail.com>
2021-09-13 08:28:13 +0000

    Make a resquest to the weatherstack API
    In order to get weather API we fire a request to the weatherstack API
    using a url that contains the longitude and latitude of my current location
    
    See: #1

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(() => {
-    console.log('2 second timer')
-}, 2000)
-
-setTimeout(() => {
-    console.log('0 second timer')
-}, 0)
-
-
-console.log('stopping')
+const url = 'http://api.weatherstack.com/current?access_key=83feab2a74815c77e3bc05ec4ddb999d&query=6.1375,1.2123'
+request({url: url}, (err, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
