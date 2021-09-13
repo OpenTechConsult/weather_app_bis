98c24cfb872b02d2b5a6ddff5d7586d209de6f15
OpenTech-Consult <opentech.consult@gmail.com>
2021-09-13 12:15:35 +0000

    Exploration of the request module
    automatically parse json data using the request module
    use the parsed json data to make a forcast of the current temperature and the feels like temperature
    
    Resolve #2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const url = 'http://api.weatherstack.com/current?access_key=83feab2a74815c77e3bc05ec4ddb999d&query=6.1375,1.2123'
-request({url: url}, (err, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url: url, json: true}, (err, response) => {
+    const currentWeatherData = response.body.current
+    console.log(`It is currently ${currentWeatherData.temperature} degrees out. It feels like ${currentWeatherData.feelslike}.`)
 })
