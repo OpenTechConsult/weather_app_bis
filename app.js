72afa26c646daec0af32a6852e87552790e98760
OpenTech-Consult <opentech.consult@gmail.com>
2021-09-15 22:55:30 +0000

    Error handling for geocoding request
    provide error handling code for the request to the geocode API
    1. Setup error handler for low-level errors
    2. Setup error handling for no matching results
    
    See #7

--- app.js
+++ app.js
@@ -17,9 +17,16 @@ request({ url: weatherStackUrl, json: true}, (err, response) => {
 
 const geocodeUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoib3BlbnRlY2hjb25zdWx0IiwiYSI6ImNrdGozamRncTE3eHEyb2pvdTcyMTB3YTAifQ._G-Ff_bRHIW5AuuS1Miipg`
 
-// request({url: geocodeUrl, json: true}, (err, response) => {
-//     const place_name = response.body.features[0].place_name
-//     const [longitude, latitude] = response.body.features[0].center
-//     console.log(`${place_name} is at longitude ${longitude} and latitude ${latitude}`)
-// })
+request({url: geocodeUrl, json: true}, (err, response) => {
+    if (err) {
+        console.log('Unable to connect to mapbox geocode API')
+    } else if (response.body.message) {
+        console.log('Location not found')
+    } else {
+        const place_name = response.body.features[0].place_name
+        const [longitude, latitude] = response.body.features[0].center
+        console.log(`${place_name} is at longitude ${longitude} and latitude ${latitude}`)
+    }
+})
+
 
