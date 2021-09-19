3ac2582f57761c19abe949bb7f111cea2d4071ee
OpenTech-Consult <opentech.consult@gmail.com>
2021-09-19 00:44:59 +0000

    Use the callback pattern to improve the weather_app application
    create a geoCode function that takes in the location address and a callback function
    Inside the geoCode function, we call the request function to call the geocode mapbox api
    By creating a function for this feature we want the code to be reusable and maintainable.
    It is also easy to do one this befor or after something else.

--- app.js
+++ app.js
@@ -1,32 +1,40 @@
-const request = require('request')
+const request = require('request');
+const geoCode = require('./utils/geocode');
 
-const weatherStackUrl = `http://api.weatherstack.com/current?access_key=83feab2a74815c77e3bc05ec4ddb999d&query=6.1375,1.2123&units=f`
+// const weatherStackUrl = `http://api.weatherstack.com/current?access_key=83feab2a74815c77e3bc05ec4ddb999d&query=6.1375,1.2123&units=f`
 
-request({ url: weatherStackUrl, json: true}, (err, response) => {
-    if (err) {
-        console.log('Unable to connect to weather API')
-    } else if (response.body.error) {
-        console.log('Unable to find location!')
-    } else {
-        const temperature = response.body.current.temperature
-        const feelsLike = response.body.current.feelslike
-        console.log(`It is ${temperature}° but it feels like ${feelsLike}°`)
-    }
+// request({ url: weatherStackUrl, json: true}, (err, response) => {
+//     if (err) {
+//         console.log('Unable to connect to weather API')
+//     } else if (response.body.error) {
+//         console.log('Unable to find location!')
+//     } else {
+//         const temperature = response.body.current.temperature
+//         const feelsLike = response.body.current.feelslike
+//         console.log(`It is ${temperature}° but it feels like ${feelsLike}°`)
+//     }
     
-})
+// })
 
-const geoCodeUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoib3BlbnRlY2hjb25zdWx0IiwiYSI6ImNrdGozamRncTE3eHEyb2pvdTcyMTB3YTAifQ._G-Ff_bRHIW5AuuS1Miipg`
+// const geoCodeUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoib3BlbnRlY2hjb25zdWx0IiwiYSI6ImNrdGozamRncTE3eHEyb2pvdTcyMTB3YTAifQ._G-Ff_bRHIW5AuuS1Miipg`
 
-request({url: geoCodeUrl, json: true}, (err, response) => {
-    if (err) {
-        console.log('Unable to connect to mapbox geocode API')
-    } else if (response.body.message) {
-        console.log('Location not found')
-    } else {
-        const place_name = response.body.features[0].place_name
-        const [longitude, latitude] = response.body.features[0].center
-        console.log(`${place_name} is at longitude ${longitude} and latitude ${latitude}`)
-    }
-})
+// request({url: geoCodeUrl, json: true}, (err, response) => {
+//     if (err) {
+//         console.log('Unable to connect to mapbox geocode API')
+//     } else if (response.body.message) {
+//         console.log('Location not found')
+//     } else {
+//         const place_name = response.body.features[0].place_name
+//         const [longitude, latitude] = response.body.features[0].center
+//         console.log(`${place_name} is at longitude ${longitude} and latitude ${latitude}`)
+//     }
+// })
+
+
+
+geoCode('Boston', (error, data) => {
+    console.log('Error', error);
+    console.log('Data', data);
+});
 
 
