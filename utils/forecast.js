a8f61370ceca8e3692610877697f023f6b71935a
OpenTech-Consult <opentech.consult@gmail.com>
2021-09-20 16:47:34 +0000

    Create a function forecast in a separate file and move the forecast fetching code from the app.js in the forecast.js file. Make the forecast function take advantage of the callback pattern. Call the forecast function inside the app.js

---
+++ utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+const forecast = (longitude, latitude, callback) => {
+    const weatherStackUrl = `http://api.weatherstack.com/current?access_key=83feab2a74815c77e3bc05ec4ddb999d&query==${longitude},${latitude}`;
+
+    request({ url: weatherStackUrl, json: true}, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weatherstack API');
+        } else if (response.body.error) {
+            callback('Unable to fetch the weather\'s forecast for the given location');
+        } else {
+            callback(undefined, {
+                temperature: response.body.current.temperature,
+                feelsLike: response.body.current.feelsLike,
+                location: response.body.location.name,
+                description: response.body.current.weather_descriptions[0]
+            });
+        }
+    });
+};
+
+module.exports = forecast;
