cc478f174fcfcb8be043b8d68732def26385f8eb
OpenTech-Consult <opentech.consult@gmail.com>
2021-09-21 13:09:44 +0000

    Chaining geocode and forecast functions
    Chained the geocode and forecast functions in order to call one after the other.
    Took care of error handling by providing error checking algorithme whenever an error occurs

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const geoCode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 geoCode('Philadelphia', (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
+    if (error) {
+        return console.log('Error', error);
+    }
+
+    forecast(data.longitude, data.latitude, (err, forecastData) => {
+        if (err) {
+            console.log('Error', err);
+        }
+        console.log('Here in ' + data.location + ' the temperature is ' + forecastData.temperature + ' degrees Celsius but it feels like ' + forecastData.feelsLike + ' degrees Celsius');
+    });
 });
 
-forecast(7788, 88999, (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
-});
+
