10ab92e7da859a3fcd610c5eb71eacb2542a9ad4
OpenTech-Consult <opentech.consult@gmail.com>
2021-09-22 15:50:05 +0000

    Refactor the weather app using object destructuring and object's property shorthand.

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ if (process.argv.length < 3) {
     console.log('A location was not provided. Try again by providing a location');
 } else if (process.argv.length >= 3) {
     const cmdLineArg = process.argv[2];
-    geoCode(cmdLineArg, (error, data) => {
+    geoCode(cmdLineArg, (error, {longitude, latitude, location} = {}) => {
         if (error) {
             return console.log('Error', error);
         }
     
-        forecast(data.longitude, data.latitude, (err, forecastData) => {
+        forecast(longitude, latitude, (err, {temperature, feelslike}) => {
             if (err) {
                 console.log('Error', err);
             }
-            console.log('Here in ' + data.location + ' the temperature is ' + forecastData.temperature + ' degrees Celsius but it feels like ' + forecastData.feelsLike + ' degrees Celsius');
+            console.log('Here in ' + location + ' the temperature is ' + temperature + ' degrees Celsius but it feels like ' + feelslike + ' degrees Celsius');
         });
     });
 }
