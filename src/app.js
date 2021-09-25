7c7d8b0cbf27a9f604d4cab92ec51a8353b0537a
OpenTech-Consult <opentech.consult@gmail.com>
2021-09-25 17:10:25 +0000

    Create two more html files
    created the about.html and help.html in the public folder
    removed the old route handler for both

--- src/app.js
+++ src/app.js
@@ -6,17 +6,6 @@ const publicDirectory = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectory));
 
-app.get('/help', (req, res) => {
-    res.send({
-        name: 'Andrew',
-        age: 27
-    });
-})
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>');
-});
-
 app.get('/weather', (req, res) => {
     res.send({
         location: { name: 'Gbadago', country: 'Togo'},
