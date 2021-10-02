9ff321cb7b2f6d482bc9b2f3460dc8f1ebcbe943
OpenTech-Consult <opentech.consult@gmail.com>
2021-10-02 00:55:25 +0000

    created the /products endpoint and the /weather endpoint
    The two endpoints show how to retrieve the query string value
    from a req object.
    Make the application displays an error

--- src/app.js
+++ src/app.js
@@ -43,11 +43,27 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'Please enter the address'
+        });
+    }
     res.send({
-        location: { name: 'Gbadago', country: 'Togo'},
-        current: { temperature: 88, humidity: 71}
+        address: req.query.address
     });
 })
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        });
+    }
+    console.log(req.query.search);
+    res.send({
+        products: []
+    });
+});
 
 app.get('/help/*', (req, res) => {
     res.render('error', {
