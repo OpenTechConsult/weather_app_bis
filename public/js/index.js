4f924614513d5d6dfcd6a3b8b2d5066ceade634b
OpenTech-Consult <opentech.consult@gmail.com>
2021-10-06 01:05:40 +0000

    create a search form that shows up on the home page.
    the form will allow users to enter address in a input text and a button to trigger the call to the fetch api to get the forecast data

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,16 @@
-fetch('http://localhost:3000/weather?address=ijjskjks')
-.then(response => response.json())
-.then(responseData => console.log(responseData)).catch((error) => console.log('Error: ' + error));
+
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = search.value;
+    if(location.length == 0) {
+        console.log('Please provide a location');
+    }
+    fetch(`http://localhost:3000/weather?address=${location}`)
+        .then(response => response.json())
+        .then(responseData => console.log(responseData)).catch((error) => console.log('Error: ' + error));
+});
+
