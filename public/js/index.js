bc9e5ba0cd5d664ca2c3b44c6a199998b0426c64
OpenTech-Consult <opentech.consult@gmail.com>
2021-10-06 14:58:05 +0000

    Display weather forecast data on the form for a given location When things go wrong, display the error message When things go right display the forecast message on the screen

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,9 @@
-
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message_one');
+const messageTwo = document.querySelector('#message_two');
+
+messageOne.textContent = '';
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -9,7 +11,18 @@ weatherForm.addEventListener('submit', (e) => {
     if(location.length == 0) {
         console.log('Please provide a location');
     }
+    messageOne.textContent = 'Loading ...';
+    messageTwo.textContent = ''
+
     fetch(`http://localhost:3000/weather?address=${location}`)
-        .then(response => response.json())
-        .then(responseData => console.log(responseData)).catch((error) => console.log('Error: ' + error));
+        .then((response) => {
+            response.json().then((data) => {
+                if(data.error) {
+                    messageOne.textContent = data.error;
+                } else {
+                    messageOne.textContent = data.location;
+                    messageTwo.textContent = data.temperature;
+                }
+            });
+        });
 });
